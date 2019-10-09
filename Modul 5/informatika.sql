376e1dcdfe883ffc0166e28dca5d8adc7622acb3
OKI KUS MAHESA <43431437+L200170064@users.noreply.github.com>
2019-10-09 19:03:51 +0700

    Add files via upload

---
+++ Modul 5/informatika.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 01, 2019 at 11:52 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `informatika`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `khs`
+-- (See below for the actual view)
+--
+CREATE TABLE `khs` (
+`NIM` varchar(10)
+,`Nama_MK` char(25)
+,`Nilai_Angka` int(10)
+,`Nilai_HUruf` char(10)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `NIM` varchar(10) NOT NULL,
+  `Nama` char(50) DEFAULT NULL,
+  `Kelas` char(5) DEFAULT NULL,
+  `Alamat` char(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`NIM`, `Nama`, `Kelas`, `Alamat`) VALUES
+('L200080001', 'Ari Wibowo', 'A', 'Solo'),
+('L200080080', 'Agustina Anggraini', 'B', 'Seragen');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `NIM` varchar(10) NOT NULL,
+  `Nama_MK` char(25) NOT NULL,
+  `Nilai_Angka` int(10) NOT NULL,
+  `Nilai_HUruf` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nilai`
+--
+
+INSERT INTO `nilai` (`NIM`, `Nama_MK`, `Nilai_Angka`, `Nilai_HUruf`) VALUES
+('L200080002', 'Kapita Selekta', 60, 'BC'),
+('L200080010', 'Pemrograman Web', 87, 'A'),
+('L200080080', 'Pemrograman Web', 90, 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `khs`
+--
+DROP TABLE IF EXISTS `khs`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `khs`  AS  select `mahasiswa`.`NIM` AS `NIM`,`nilai`.`Nama_MK` AS `Nama_MK`,`nilai`.`Nilai_Angka` AS `Nilai_Angka`,`nilai`.`Nilai_HUruf` AS `Nilai_HUruf` from (`mahasiswa` join `nilai` on((`mahasiswa`.`NIM` = `nilai`.`NIM`))) ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`NIM`);
+
+--
+-- Indexes for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`NIM`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
